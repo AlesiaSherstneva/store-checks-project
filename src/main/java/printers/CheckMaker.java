116ec92d4cc66aeba288b33129cc7bc2ed6299a0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-04-09 16:35:49 +0300

    Update CheckMaker

--- src/main/java/printers/CheckMaker.java
+++ src/main/java/printers/CheckMaker.java
@@ -7,15 +7,17 @@ import java.util.List;
 
 public class CheckMaker {
     public StringBuilder getCheck(List<Product> productList, double total, double totalDiscount) {
-        String LINE = "-------------------------------";
+        String LINE = "-".repeat(31);
 
         StringBuilder check = new StringBuilder()
                 .append(LINE).append(String.format("\n%17s", "CHECK"))
                 .append(String.format("\n%25s\n", new Date().toString().substring(0, 20)))
                 .append(LINE).append("\n");
+
         for (Product product : productList) {
             check.append(product);
         }
+
         check.append(LINE).append(String.format("\n%20s%10.2f", "TOTAL", total))
                 .append(String.format("\n%20s%10.2f\n", "total discount", totalDiscount))
                 .append(LINE).append(String.format("\n%s\n", "THANK YOU FOR CHOOSING OUR SHOP"))
